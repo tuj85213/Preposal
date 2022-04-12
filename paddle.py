4a5be52122018fb3a500779fbd28a9ae47ce9648
SiddhiVinay <102323180+SiddhiVinay@users.noreply.github.com>
2022-04-12 15:38:21 -0400

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -3,17 +3,19 @@
 #paddles
 
 import pygame
-black = (0,0,0)
-
+BLACK = (0,0,0)
+ 
 class Paddle(pygame.sprite.Sprite):
-
-    def _init_(self, color, width, height):
-        super()._init_()
-
+    
+    def __init__(self, color, width, height):
+        super().__init__()
+       
         self.image = pygame.Surface([width, height])
-        self.image.fill(black)
-        self.image.set_colorkey(black)
-
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+ 
+        # Draw the paddle (a rectangle!)
         pygame.draw.rect(self.image, color, [0, 0, width, height])
-
+        
+        # Fetch the rectangle object that has the dimensions of the image.
         self.rect = self.image.get_rect()
